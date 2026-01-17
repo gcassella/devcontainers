2325a677b137b4bbcef1552512bca92d27239e54
gwmcassella <gwmcassella@gmail.com>
2026-01-17 21:03:57 +0000

    Create Python devcontainer setup
    This devcontainer setup includes
    
    - Setting up `gh` CLI auth
    - Setting up `gemini` CLI auth
    - Installation of `uv`
    - Installation of `moor` pager
    - Installation of `fzf`
    - Installation of `sl` (Sapling-SCM)
    - Preserves bash history between sessions
    - Installation of `ruff` and `ty`
    - Extensions:
      - Jupyter
      - Python
      - VSCode icons
      - Gemini CLI Companion
      - Ty
      - Ruff
      - Sapling SCM

---
+++ .devcontainer/init-command.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# If GitHub CLI is installed then create a temporary token store used for gh auth.
+if command -v gh >/dev/null 2>&1 && gh auth token >/dev/null 2>&1; then
+    echo "$(gh auth token)" > .devcontainer/gh_token.txt;
+fi
+
+# If GEMINI_API_KEY present on host, create a temporary key store (deleted after run).
+if [ -n \"${GEMINI_API_KEY}\" ]; then 
+    echo ${GEMINI_API_KEY} > .devcontainer/gemini_key.txt;
+fi
---
+++ .devcontainer/post-attach-command.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Clean up injected secrets in post-attach as initCommand may run on attach.
+
+if [ -f .devcontainer/gh_token.txt ]; then
+    # Delete gh_token.txt to prevent any accidental leakage.
+    rm .devcontainer/gh_token.txt;
+fi
+
+if [ -f .devcontainer/gemini_key.txt ]; then 
+    # Delete gemini_key.txt to prevent any accidental leakage.
+    rm .devcontainer/gemini_key.txt;
+fi
