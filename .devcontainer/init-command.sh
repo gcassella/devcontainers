a107598e4b38f31b69cdae737ec7d5d6930e7652
gwmcassella <gwmcassella@gmail.com>
2026-01-17 23:00:59 +0000

    Call `uv sync` before first start to instantiate venv

--- .devcontainer/init-command.sh
+++ .devcontainer/init-command.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Make sure venv is instantiated before first run
+uv sync
+
 # If GitHub CLI is installed then create a temporary token store used for gh auth.
 if command -v gh >/dev/null 2>&1 && gh auth token >/dev/null 2>&1; then
     echo "$(gh auth token)" > .devcontainer/gh_token.txt;
